8ab1e3a2f4d1ccb3eb2ecdf094e09e702a9df429
Saurav Das <xaurvdas01@gmail.com>
2021-02-12 21:33:06 +0530

    dontKnow

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -5,20 +5,23 @@ import {
     DrawerHeader,
     DrawerOverlay,
     DrawerContent,
-    DrawerCloseButton,
     useDisclosure,
     Button,
-    Tooltip,
     MenuButton,
     MenuList,
     MenuItem,
     Menu,
+    Stack,
   } from "@chakra-ui/react";
   import {RiMenu2Line} from 'react-icons/ri';
   import {BsChevronExpand} from 'react-icons/bs';
+  import {HiChevronDown} from 'react-icons/hi';
+  import {VscVmActive} from 'react-icons/vsc';
+
 
   import "./SideBar.css"
 import React from 'react'
+import AddWorkBook from "../AddWorkBook/AddWorkBook";
 const SideBar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     return ( <>
@@ -37,24 +40,77 @@ const SideBar = () => {
         <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay>
             <DrawerContent>
-              <DrawerHeader bg="brand.dark">
-              <Tooltip label="WorkSpaces" fontSize="md">
+              <DrawerHeader display="flex"
+              alignItems="center"
+              justifyContent="center"
+              bg="brand.dark">
+         
               <Menu>
-                  <MenuButton as={Button} rightIcon={<BsChevronExpand />}>
-                    Actions
+                  <MenuButton as={Button} 
+                  _hover={{ bg: "transparent" }}
+                  width="100%"
+                  _active={{
+                    bg: "transparent",
+                    transform: "scale(0.98)",
+                    borderColor: "#bec3c9",
+                  }}
+                  _focus={{
+                    boxShadow:
+                      "0 0 0px 0px rgba(88, 144, 255, 0), 0 1px 1px rgba(0, 0, 0, 0)",
+                  }}
+                  borderRadius="0px" 
+                  bg="brand.dark"
+                  color="white"
+                  leftIcon={<VscVmActive color="green"/>}
+                  rightIcon={<BsChevronExpand />}>
+                    WEB DEV
                   </MenuButton>
-                  <MenuList>
-                    <MenuItem>Download</MenuItem>
-                    <MenuItem>Create a Copy</MenuItem>
-                    <MenuItem>Mark as Draft</MenuItem>
-                    <MenuItem>Delete</MenuItem>
-                    <MenuItem>Attend a Workshop</MenuItem>
+                  <MenuList bg="brand.light" borderRadius="none" 
+                 
+                  ml="3">
+                    <MenuItem >Machine Learning</MenuItem>
+                    <MenuItem>Competitive Programing</MenuItem>
+            
                   </MenuList>
                 </Menu>
-              </Tooltip>
+          
               </DrawerHeader>
               <DrawerBody bg="brand.dark">
-             
+
+              
+
+              <Stack spacing="10px">
+                <AddWorkBook/>
+              <Menu>
+                  <MenuButton as={Button} 
+                  _hover={{ bg: "transparent" }}
+                  width="100%"
+                  _active={{
+                    bg: "transparent",
+                    transform: "scale(0.98)",
+                    borderColor: "#bec3c9",
+                  }}
+                  _focus={{
+                    boxShadow:
+                      "0 0 0px 0px rgba(88, 144, 255, 0), 0 1px 1px rgba(0, 0, 0, 0)",
+                  }}
+                  borderRadius="0px" 
+                  bg="brand.dark"
+                  color="white"
+            
+                  rightIcon={<HiChevronDown />}>
+                    WEB DEV
+                  </MenuButton>
+                  <MenuList bg="brand.light" borderRadius="none" 
+                 
+                  ml="3">
+                    <MenuItem >Machine Learning</MenuItem>
+                    <MenuItem>Competitive Programing</MenuItem>
+            
+                  </MenuList>
+                </Menu>
+          
+              </Stack>
               </DrawerBody>
             </DrawerContent>
           </DrawerOverlay>
