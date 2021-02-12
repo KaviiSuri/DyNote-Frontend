3a8a037416352a5a5860fac662d41685ef4f02e7
Saurav Das <xaurvdas01@gmail.com>
2021-02-12 18:03:06 +0530

    package

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Switch>
+      {/* <Route path="/home" component={Home} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/not-found" /> */}
+    </Switch>
+  );
 }
 
 export default App;
