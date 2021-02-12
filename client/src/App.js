60ca80affeaed1b4ca16bfebc925767a48ef32e6
Saurav Das <xaurvdas01@gmail.com>
2021-02-12 18:13:00 +0530

    basic_structure_Done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/home";
+import WorkBook from "./pages/WorkBook/workbook";
 
 function App() {
   return (
     <Switch>
-      {/* <Route path="/home" component={Home} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect from="/" exact to="/home" />
-        <Redirect to="/not-found" /> */}
+      <Route path="/home" component={Home} />
+      <Route path="/workspace" component={WorkBook} />
+      {/* <Route path="/not-found" component={NotFound} /> */}
+      <Redirect from="/" exact to="/home" />
+      {/* <Redirect to="/not-found" /> */}
     </Switch>
   );
 }
---
+++ client/src/pages/WorkBook/workbook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./workbook.css"
+const WorkBook = () => {
+    return ( <>
+    WorkBook
+    </> );
+}
+ 
+export default WorkBook;
