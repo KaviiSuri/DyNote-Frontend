f896eb8b123a83ec7737d6ecba7774fb9fd43c97
KaviiSuri <surikavii@gmail.com>
2021-02-13 16:29:01 +0530

    completed workspace provider

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import "./App.css";
 import Home from "./pages/Home/home";
 import WorkSpace from "./pages/WorkSpace/workspace";
 import { AuthProvider } from "./providers/authProvider";
+import { WorkspaceProvider } from "./providers/workspaceProvider";
 
 function App() {
   console.log(process.env.FIREBASE_WEB_CONFIG_BASE64);
   return (
     <AuthProvider>
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/workspace" component={WorkSpace} />
-        {/* <Route path="/not-found" component={NotFound} /> */}
-        <Redirect from="/" exact to="/home" />
-        {/* <Redirect to="/not-found" /> */}
-      </Switch>
+      <WorkspaceProvider>
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/workspace" component={WorkSpace} />
+          {/* <Route path="/not-found" component={NotFound} /> */}
+          <Redirect from="/" exact to="/home" />
+          {/* <Redirect to="/not-found" /> */}
+        </Switch>
+      </WorkspaceProvider>
     </AuthProvider>
   );
 }
--- client/src/providers/workspaceProvider.js
+++ client/src/providers/workspaceProvider.js
@@ -1,23 +1,70 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+import { rootUrl } from "../config";
+import { useAuth } from "../providers/authProvider";
+
 const WorkspaceContext = React.createContext();
-const WorkspaceUpdateContext = React.createContext();
+// const WorkspaceUpdateContext = React.createContext();
 // exports
 export function useWorkspace() {
   return useContext(WorkspaceContext);
 }
-export function useWorkspaceUpdate() {
-  return useContext(WorkspaceUpdateContext);
-}
+// export function useWorkspaceUpdate() {
+//   return useContext(WorkspaceUpdateContext);
+// }
 export function WorkspaceProvider({ children }) {
   // States
+  const { firebaseUser, backendUser } = useAuth();
+  const [currentWorkspaceId, setCurrentWorkspaceId] = useState();
+  const [workspaceData, setWorkspaceData] = useState();
 
   // functionalities
+  const fetchData = async (currentWorkspaceId) => {
+    try {
+      const { data } = axios.get(
+        `${rootUrl}/workspace/${currentWorkspaceId}`,
+        {},
+        {
+          headers: {
+            firebase_token: await firebaseUser.getIdToken(),
+          },
+        }
+      );
+      setWorkspaceData(data);
+    } catch (error) {
+      console.log(error);
+      console.log(error.response.data);
+    }
+  };
+
+  //==== effects ====
+  //effect to refetch workspace when change in id
+  useEffect(() => {
+    if (currentWorkspaceId) {
+      fetchData(currentWorkspaceId);
+    }
+  }, [currentWorkspaceId]);
+
+  // effect to change id to default if invalid id or user changed
+  useEffect(() => {
+    if (backendUser) {
+      if (
+        !currentWorkspaceId ||
+        !backendUser.workspaces.includes(currentWorkspaceId)
+      ) {
+        console.log("Backend User From workspace", backendUser);
+        setCurrentWorkspaceId(backendUser.workspaces[0]);
+      }
+    }
+  }, [backendUser]);
 
   return (
-    <WorkspaceContext.Provider value={}>
-      <WorkspaceUpdateContext.Provider value={}>
-        {children}
-      </WorkspaceUpdateContext.Provider>
+    <WorkspaceContext.Provider
+      value={{ currentWorkspaceId, workspaceData, setCurrentWorkspaceId }}
+    >
+      {/* <WorkspaceUpdateContext.Provider value={}> */}
+      {children}
+      {/* </WorkspaceUpdateContext.Provider> */}
     </WorkspaceContext.Provider>
   );
 }
